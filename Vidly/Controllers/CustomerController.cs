87af64210c44eb0464b25e477471e8a53b120b0c
GuptaG <GuptaG@LT-GUPTAG>
2017-08-14 15:13:58 +0530

    Add project files.

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        [Route("Customer/CustomerList")]
+        public ActionResult CustomerList()
+        {
+            var customers = GetCustomers();
+            var viewModel = new ListCustomerViewModel()
+            {
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        [Route("Customer/Details/{id}")]
+        public ActionResult Details(int id) {
+            if (string.IsNullOrEmpty(GetCustomerName(id)))
+            {
+                return HttpNotFound();
+            }
+            else {
+                return Content("Customer Name:: " + GetCustomerName(id));
+            }
+        }
+
+        private string GetCustomerName(int id) {
+            string customerName = "";
+            var customers = GetCustomers();
+            foreach (Customer c in customers) {
+                if (c.Id == id)
+                    customerName = c.Name;
+            }
+            return customerName;
+        }
+
+        private IEnumerable<Customer> GetCustomers() {
+            return new List<Customer>()
+            {
+                new Customer() {  Name = "Jyotnain", Id = 1 },
+                new Customer() { Name = "Gaurav", Id = 2 }
+            };
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        /*public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "DDLJ" };
+            var customers = new List<Customer>
+            {
+                new Customer() { Name = "Kabir" },
+                new Customer() { Name = "Lallu" }
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Kabir's Id " + id);
+        }
+
+        [Route("movies/released/{year}/{month:range(1,12):regex(\\d{2})}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content("Year::  " + year + " and Month:  " + month);
+        }*/
+
+        [Route("Movie/MovieList")]
+        public ActionResult MovieList()
+        {
+            var movies = GetMovies();
+            var viewModel = new ListMoviesViewModel()
+            {
+                Movies = movies
+            };
+            return View(viewModel);
+        }
+
+        private IEnumerable<Movie> GetMovies() {
+            return new List<Movie>()
+            {
+                new Movie() { Name = "Jatt and Juliet", Id = 1 },
+                new Movie() { Name = "Godzilla", Id = 2 }
+            };
+        }
+    }
+}
